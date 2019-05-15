8c6aa419a08e6bc92e22c3cb9ce1f58ef11ccaad
Vishal600 <48982721+Vishal600@users.noreply.github.com>
2019-05-15 18:02:58 +0530

    Update Task- 1.cs

--- SortingPeople/Task- 1.cs
+++ SortingPeople/Task- 1.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PeopleTask1;
 
 namespace PeopleTask1
 {
@@ -11,73 +12,74 @@ namespace PeopleTask1
         static void Main(string[] args)
         {
             int counter = 0;
-
             List<People> Person = new List<People>();
             Person.Add(new People());
-            Person[0].firstName = "Rahul";
-            Person[0].lastName = "Ganesh";
+            Person[0].firstName = "David";
+            Person[0].lastName = "Warner";
             Person[0].Age = 80;
-            Person[0].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[0].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";//
 
             Person.Add(new People());
-            Person[1].firstName = "BJ";
-            Person[1].lastName = "GaneshMoorthy";
+            Person[1].firstName = "Shikhar";
+            Person[1].lastName = "Dhawan";
             Person[1].Age = 25;
-            Person[1].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
-
-
-
+            Person[1].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[2].firstName = "Man";
-            Person[2].lastName = "Poliant";
+            Person[2].firstName = "KL";
+            Person[2].lastName = "Rahul";
             Person[2].Age = 17;
-            Person[2].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[2].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
 
             Person.Add(new People());
-            Person[3].firstName = "Hardik";
-            Person[3].lastName = "Pandya";
+            Person[3].firstName = "Rishabh";
+            Person[3].lastName = "Pant";
             Person[3].Age = 19;
-            Person[3].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[3].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
 
             Person.Add(new People());
-            Person[4].firstName = "Simon";
-            Person[4].lastName = "Jones";
+            Person[4].firstName = "Mahinder Singh";
+            Person[4].lastName = "Dhoni";
             Person[4].Age = 65;
-            Person[4].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[4].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[5].firstName = "Ganesh";
-            Person[5].lastName = "Panesh";
+            Person[5].firstName = "Andre";
+            Person[5].lastName = "Russel";
             Person[5].Age = 25;
-            Person[5].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[5].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[6].firstName = "Halal";
-            Person[6].lastName = "Haludeen";
+            Person[6].firstName = "Hardik";
+            Person[6].lastName = "Pandya";
             Person[6].Age = 33;
-            Person[6].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[6].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[7].firstName = "Kedar";
-            Person[7].lastName = "Jadhav";
+            Person[7].firstName = "Shreyas";
+            Person[7].lastName = "Gopal";
             Person[7].Age = 75;
-            Person[7].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[7].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[8].firstName = "Kane";
-            Person[8].lastName = "Williamson";
+            Person[8].firstName = "Kagiso";
+            Person[8].lastName = "Rabada";
             Person[8].Age = 45;
-            Person[8].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[8].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Person.Add(new People());
-            Person[9].firstName = "Yelawolf";
-            Person[9].lastName = "Catfish Billy";
+            Person[9].firstName = "Imran";
+            Person[9].lastName = "Tahir";
             Person[9].Age = 41;
-            Person[9].flag = Person[0].Age <= 19 ? "teenager" : "Not a teen";
+            Person[9].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
+            Person.Add(new People());
+            Person[10].firstName = "Jasprit";
+            Person[10].lastName = "Bumrah";
+            Person[10].Age = 39;
+            Person[10].flag = null; //Person[0].Age <= 19 ? "teenager" : "Not a teen";
 
             Console.WriteLine("The following are the senior citizens \n");
             foreach (People opeople in Person)
@@ -124,11 +126,19 @@ namespace PeopleTask1
 
             foreach (People opeople in Person)
             {
-                Console.WriteLine(opeople.firstName + " " + opeople.lastName);
+                opeople.flag = opeople.Age <= 19 ? "Teenager" : "No";
+                
+                /*Console.WriteLine(opeople.firstName + " " + opeople.lastName);
                 Console.WriteLine("this person is a" + " " + opeople.flag);
-                Console.WriteLine("\n");
+                Console.WriteLine("\n");*/
             }
+            Console.WriteLine("\nThese are the Teens  \n");
+            foreach (var opeople in Person)
+            {
+                if (opeople.flag == "Teenager")
+                    Console.WriteLine(opeople.firstName + " " + opeople.lastName + " " +"is a" + " "+opeople.flag);
 
+            }
             Console.ReadLine();
 
 
